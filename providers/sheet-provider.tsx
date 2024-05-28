c647f2955b09c077043b189ff8cf232fc7b14f64
AllTheCodeWeCannotSee <183735517@qq.com>
2024-05-28 09:31:22 +0800

    categories

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { useMountedState } from "react-use";
 
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet"
-import { useMountedState } from "react-use";
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+
 
 
 export const SheetProvider = () => {
@@ -13,6 +16,8 @@ export const SheetProvider = () => {
         <>
             <NewAccountSheet />
             <EditAccountSheet />
+            <NewCategorySheet />
+            <EditCategorySheet />
         </>
     )
 }
